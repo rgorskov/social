d1630206b2564eca0e9649e37be1e0bf227644f0
rgorskov <rgorskov@gmail.com>
2020-04-23 22:10:30 +0500

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/b/be/Lineage_OS_Logo.png"
+        alt=""
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <main className="main">
+      <div>
+        <img
+          src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"
+          alt=""
+        />
+      </div>
+      <div>ava + description</div>
+      <div>
+        my posts
+        <div>new post</div>
+        <div>
+          posts
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Profile;

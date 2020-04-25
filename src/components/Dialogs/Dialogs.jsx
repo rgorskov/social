a16046464b5d4d8be5761d3b68ea083a1c78dcc0
rgorskov <rgorskov@gmail.com>
2020-04-25 21:28:05 +0500

    dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,17 @@ import style from "./Dialogs.module.css";
 
 const Dialogs = () => {
   return (
-    <div>
-      Dialogs
+    <div className={style.dialogs}>
+      <div className={style.usersList}>
+        <div className={`${style.userDialog} ${style.active}`}>Yury</div>
+        <div className={style.userDialog}>Dima</div>
+        <div className={style.userDialog}>Oleg</div>
+      </div>
+      <div className={style.messages}>
+        <div className={style.message}>Hello</div>
+        <div className={style.message}>How are you?</div>
+        <div className={style.message}>kek</div>
+      </div>
     </div>
   );
 };

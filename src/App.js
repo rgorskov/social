45a5f66be4aecf203cd4e312aff25a104f7bac44
rgorskov <rgorskov@gmail.com>
2020-04-28 00:27:51 +0500

    move BrowserRouter to index.js

--- src/App.js
+++ src/App.js
@@ -7,30 +7,33 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import Settings from "./components/Settings/Settings";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 function App({state}) {
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <header className="header">
-          <Header />
-        </header>
+    <div className="app-wrapper">
+      <header className="header">
+        <Header />
+      </header>
 
-        <aside className="navbar">
-          <Navbar />
-        </aside>
+      <aside className="navbar">
+        <Navbar />
+      </aside>
 
-        <main className="main">
-          <Route render={ () => <Profile data={state.profilePage} /> } path='/profile' />
-          <Route render={ () => <Dialogs data={state.dialogsPage} /> } path='/dialogs' />
-          <Route render={ () => <Settings /> } path='/settings' />
-          <Route render={ () => <Music /> } path='/music' />
-          <Route render={ () => <News /> } path='/news' />
-          
-        </main>
-      </div>
-    </BrowserRouter>
+      <main className="main">
+        <Route
+          render={() => <Profile data={state.profilePage} />}
+          path="/profile"
+        />
+        <Route
+          render={() => <Dialogs data={state.dialogsPage} />}
+          path="/dialogs"
+        />
+        <Route render={() => <Settings />} path="/settings" />
+        <Route render={() => <Music />} path="/music" />
+        <Route render={() => <News />} path="/news" />
+      </main>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import state from './redux/state';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import state from "./redux/state";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App state={state} />
+    <BrowserRouter>
+      <App state={state} />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change

e6ec98fd12f14b122cdc6fae630b689d3fc6fc96
rgorskov <rgorskov@gmail.com>
2020-04-29 22:33:13 +0500

    move state and func-s to store

--- src/index.js
+++ src/index.js
@@ -3,23 +3,28 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import state from "./redux/state";
-import { addPost, sendMessage, updatePostText, updateMessageText,subscribe } from "./redux/state";
+import store from "./redux/state";
 import { BrowserRouter } from "react-router-dom";
 
-const rerenderEntireTree = (state) => {
-    ReactDOM.render(
-      <React.StrictMode>
-        <BrowserRouter>
-          <App state={state} addPost={addPost} updatePostText={updatePostText} sendMessage={sendMessage} updateMessageText={updateMessageText} />
-        </BrowserRouter>
-      </React.StrictMode>,
-      document.getElementById("root")
-    );
-  };
-  
-  rerenderEntireTree(state);
-  subscribe(rerenderEntireTree);
+const rerenderEntireTree = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App
+          state={store.getState()}
+          addPost={store.addPost.bind(store)}
+          updatePostText={store.updatePostText.bind(store)}
+          sendMessage={store.sendMessage.bind(store)}
+          updateMessageText={store.updateMessageText.bind(store)}
+        />
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
+
+rerenderEntireTree();
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,68 +1,65 @@
-let rerenderEntireTree = () => {};
 
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: "Hello", likesCount: 10 },
-      { id: 2, message: "How are you?", likesCount: 2 },
-      { id: 3, message: "Noice", likesCount: 5 },
-    ],
-    currentText: ''
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Hello", likesCount: 10 },
+        { id: 2, message: "How are you?", likesCount: 2 },
+        { id: 3, message: "Noice", likesCount: 5 },
+      ],
+      currentText: ''
+    },
+    dialogsPage: {
+      usersDialogs: [
+        { id: 1, name: "Yury" },
+        { id: 2, name: "Dima" },
+        { id: 3, name: "Oleg" },
+      ],
+      messages: [
+        { id: 1, text: "Hello" },
+        { id: 2, text: "How are you?" },
+      ],
+      currentText: ''
+    },
   },
-  dialogsPage: {
-    usersDialogs: [
-      { id: 1, name: "Yury" },
-      { id: 2, name: "Dima" },
-      { id: 3, name: "Oleg" },
-    ],
-    messages: [
-      { id: 1, text: "Hello" },
-      { id: 2, text: "How are you?" },
-    ],
-    currentText: ''
+  getState() { return this._state; },
+  _subscriber() {},
+  addPost() {
+    let id = this._state.profilePage.posts.reduce((max, curr) => curr.id > max ? curr.id : max, 0) + 1;
+    let newPost = {
+      id, 
+      message: this._state.profilePage.currentText, 
+      likesCount: 0
+    };
+  
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.currentText = '';
+  
+    this._subscriber(this._state);
+  },
+  sendMessage() {
+    let id = this._state.dialogsPage.messages.reduce((max, curr) => curr.id > max ? curr.id : max, 0) + 1;
+    let newMessage = {
+      id, 
+      text: this._state.dialogsPage.currentText
+    };
+    
+    this._state.dialogsPage.messages.push(newMessage);
+    this._state.dialogsPage.currentText = '';
+    
+    this._subscriber(this._state);
+  },
+  updatePostText(text) {
+    this._state.profilePage.currentText = text;
+    this._subscriber(this._state);
+  },
+  updateMessageText(text) {
+    this._state.dialogsPage.currentText = text;
+    this._subscriber(this._state);
+  },
+  subscribe(observer) {
+    this._subscriber = observer;
   },
 };
-window.state = state;
 
-export const addPost = () => {
-  let id = state.profilePage.posts.reduce((max, curr) => curr.id > max ? curr.id : max, 0) + 1;
-  let newPost = {
-    id, 
-    message: state.profilePage.currentText, 
-    likesCount: 0
-  };
-
-  state.profilePage.posts.push(newPost);
-  state.profilePage.currentText = '';
-
-  rerenderEntireTree(state);
-};
-
-export const sendMessage = () => {
-  let id = state.dialogsPage.messages.reduce((max, curr) => curr.id > max ? curr.id : max, 0) + 1;
-  let newMessage = {
-    id, 
-    text: state.dialogsPage.currentText
-  };
-  
-  state.dialogsPage.messages.push(newMessage);
-  state.dialogsPage.currentText = '';
-  
-  rerenderEntireTree(state);
-};
-
-export const updatePostText = (text) => {
-  state.profilePage.currentText = text;
-  rerenderEntireTree(state);
-};
-
-export const updateMessageText = (text) => {
-  state.dialogsPage.currentText = text;
-  rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-};
-
-export default state;
+export default store;

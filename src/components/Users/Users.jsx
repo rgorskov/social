6dd7b9a0ade7afb992d04522345e599e4dd98423
rgorskov <rgorskov@gmail.com>
2020-05-05 13:38:32 +0500

    users loading from api

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import style from "./Users.module.css";
-
-let initialUsers = [
-  { id: 1, followed: false, firstname: 'Alexey', surname: 'Popov', photoUrl: 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg' },
-  { id: 2, followed: true, firstname: 'Sergey', surname: 'Petrov', photoUrl: 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg' },
-  { id: 3, followed: false, firstname: 'Petr', surname: 'Ivanov', photoUrl: 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg' },
-  { id: 4, followed: false, firstname: 'Ivan', surname: 'Kukanov', photoUrl: 'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg' }
-];
+import * as axios from 'axios';
+import defaultUserPhoto from '../../assets/images/user-photo.png';
 
 const Users = ({ users, setFollow, setUsers }) => {
   if(!users.length) {
-    setUsers(initialUsers);
+    axios.get('https://social-network.samuraijs.com/api/1.0/users')
+      .then(response => {
+        setUsers(response.data.items);
+      });
   }
 
   return (
@@ -19,10 +17,10 @@ const Users = ({ users, setFollow, setUsers }) => {
         return (
           <div className={style.item} key={u.id}>
             <div className={style.photo}>
-              <img src={u.photoUrl} alt="User photo" />
+              <img src={(u.photos.small !== null) ? u.photos.small : defaultUserPhoto} alt="User photo" />
             </div>
             <div className={style.info}>
-              <p className={style.name}>{u.firstname + " " + u.surname}</p>
+              <p className={style.name}>{u.name}</p>
               <button className="btn" onClick={() => setFollow(u.id, !u.followed)}>
                 {u.followed ? "Unfollow" : "Follow"}
               </button>

caa290abdfd365a07af70db96dfc7ebc865e619b
rgorskov <rgorskov@gmail.com>
2020-05-05 15:15:45 +0500

    users remake to class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,35 +1,79 @@
 import React from "react";
 import style from "./Users.module.css";
-import * as axios from 'axios';
-import defaultUserPhoto from '../../assets/images/user-photo.png';
+import * as axios from "axios";
+import defaultUserPhoto from "../../assets/images/user-photo.png";
 
-const Users = ({ users, setFollow, setUsers }) => {
-  if(!users.length) {
-    axios.get('https://social-network.samuraijs.com/api/1.0/users')
-      .then(response => {
-        setUsers(response.data.items);
-      });
+// const Users = ({ users, setFollow, setUsers }) => {
+//   if(!users.length) {
+//     axios.get('https://social-network.samuraijs.com/api/1.0/users')
+//       .then(response => {
+//         setUsers(response.data.items);
+//       });
+//   }
+
+//   return (
+//     <div className={style.users}>
+//       {users.map((u) => {
+//         return (
+//           <div className={style.item} key={u.id}>
+//             <div className={style.photo}>
+//               <img src={(u.photos.small !== null) ? u.photos.small : defaultUserPhoto} alt="User photo" />
+//             </div>
+//             <div className={style.info}>
+//               <p className={style.name}>{u.name}</p>
+//               <button className="btn" onClick={() => setFollow(u.id, !u.followed)}>
+//                 {u.followed ? "Unfollow" : "Follow"}
+//               </button>
+//             </div>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// };
+
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+
+    if (!this.props.users.length) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => {
+          this.props.setUsers(response.data.items);
+        });
+    }
   }
 
-  return (
-    <div className={style.users}>
-      {users.map((u) => {
-        return (
-          <div className={style.item} key={u.id}>
-            <div className={style.photo}>
-              <img src={(u.photos.small !== null) ? u.photos.small : defaultUserPhoto} alt="User photo" />
+  render() {
+    return (
+      <div className={style.users}>
+        {this.props.users.map((u) => {
+          return (
+            <div className={style.item} key={u.id}>
+              <div className={style.photo}>
+                <img
+                  src={
+                    u.photos.small !== null ? u.photos.small : defaultUserPhoto
+                  }
+                  alt="User photo"
+                />
+              </div>
+              <div className={style.info}>
+                <p className={style.name}>{u.name}</p>
+                <button
+                  className="btn"
+                  onClick={() => this.props.setFollow(u.id, !u.followed)}
+                >
+                  {u.followed ? "Unfollow" : "Follow"}
+                </button>
+              </div>
             </div>
-            <div className={style.info}>
-              <p className={style.name}>{u.name}</p>
-              <button className="btn" onClick={() => setFollow(u.id, !u.followed)}>
-                {u.followed ? "Unfollow" : "Follow"}
-              </button>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+          );
+        })}
+      </div>
+    );
+  }
+}
 
 export default Users;

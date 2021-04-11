e88d8fa26b52130e36616020ebf3a2181e605c08
rgorskov <rgorskov@gmail.com>
2021-04-11 17:35:30 +0500

    remove store subscribe

--- src/index.js
+++ src/index.js
@@ -2,28 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import store from './data/store';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-const rerenderEntireTree = () => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-};
-
-rerenderEntireTree();
-store.subscribe(rerenderEntireTree);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
